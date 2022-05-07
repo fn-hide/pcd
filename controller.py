4fb139ecdeaba463cf1f14ade1daabd93c918ff3
fn-hide <febriantohuda@gmail.com>
2022-05-07 18:41:44 +0700

    menambahkan tampilan histogram

--- controller.py
+++ controller.py
@@ -10,6 +10,9 @@ from PyQt5.QtGui import (
 from functools import partial
 
 import cv2 as cv
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 
 
 
@@ -40,9 +43,32 @@ class Controller:
         self._view.imgLabel.setPixmap(
             QPixmap().fromImage(self.img).scaled(self._view.imgLabel.size(), Qt.KeepAspectRatio)
         )
-        print(imageFile)
-        self._model._img = cv.imread(imageFile)
         
+        self.fig = plt.figure()
+        cnv = FigureCanvasQTAgg(self.fig)
+        
+        # Produce histogram of grayscale image
+        self._model._img = cv.imread(imageFile, 0)
+        histr = cv.calcHist([self._model._img], [0], None, [256], [0, 256])
+        plt.plot(histr)
+        plt.show()
+        
+        # # Produce histogram of RGB image
+        # self._model._img = cv.imread(imageFile)
+        # color = ('b', 'g', 'r')
+        # for i, col in enumerate(color):
+        #     histr = cv.calcHist([self._model._img], [i], None, [256], [0, 256])
+        #     plt.plot(histr, color=col)
+        #     plt.xlim([0, 256])
+        
+        cnv.draw()
+        cnvSize = cnv.size()
+        cnvWidth, cnvHeight = cnvSize.width(), cnvSize.height()
+        cnvImg = QImage(cnv.buffer_rgba(), cnvWidth, cnvHeight, QImage.Format_ARGB32)
+        self._view.hisLabel.setPixmap(
+            QPixmap(cnvImg).scaled(self._view.hisLabel.size(), Qt.KeepAspectRatio)
+        )
+
         
     def closeProgram(self, event):
         msg = 'Are you sure to quit?'
